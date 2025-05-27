d5af91f76f45c949324032cf739c39886ce6824d
ALX-18 <148252959+ALX-18@users.noreply.github.com>
2025-05-27 21:44:19 +0200

    test

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = "https://cocktail-six-indol.vercel.app/api";
 const USE_MOCK_API = false;
 
 // Liste des ingrédients (à charger depuis le fichier ou à définir ici)
